7da42946b75ba7334be567e2580823caf899f068
nathan <side_nathan@hotmail.com>
2025-04-19 20:27:18 -0400

    ok

--- medfetch/dist/sqlite-wasm/index.mjs
+++ medfetch/dist/sqlite-wasm/index.mjs
@@ -1,17 +1,17 @@
-var v = (t) => {
+var E = (t) => {
   throw TypeError(t);
 };
-var g = (t, e, r) => e.has(t) || v("Cannot " + r);
-var a = (t, e, r) => (g(t, e, "read from private field"), r ? r.call(t) : e.get(t)), w = (t, e, r) => e.has(t) ? v("Cannot add the same private member more than once") : e instanceof WeakSet ? e.add(t) : e.set(t, r), k = (t, e, r, o) => (g(t, e, "write to private field"), o ? o.call(t, r) : e.set(t, r), r), b = (t, e, r) => (g(t, e, "access private method"), r);
-import { Effect as d, Data as S } from "effect";
+var g = (t, e, r) => e.has(t) || E("Cannot " + r);
+var d = (t, e, r) => (g(t, e, "read from private field"), r ? r.call(t) : e.get(t)), p = (t, e, r) => e.has(t) ? E("Cannot add the same private member more than once") : e instanceof WeakSet ? e.add(t) : e.set(t, r), k = (t, e, r, s) => (g(t, e, "write to private field"), s ? s.call(t, r) : e.set(t, r), r), b = (t, e, r) => (g(t, e, "access private method"), r);
+import { Effect as a, Data as S } from "effect";
 import "@sqlite.org/sqlite-wasm";
 import { MESSAGE_TYPES as x } from "./types.mjs";
-var f, u, p, E;
+var f, u, w, v;
 class I {
   constructor() {
-    w(this, p);
-    w(this, f);
-    w(this, u);
+    p(this, w);
+    p(this, f);
+    p(this, u);
     k(this, f, /* @__PURE__ */ new Map()), k(this, u, Object.fromEntries(
       x.map((e) => [e, 0])
     ));
@@ -23,8 +23,8 @@ class I {
    * @returns How many times msgType has been called including the ++
    */
   increment(e) {
-    const r = a(this, u)[e] = (a(this, u)[e] || 0) + 1;
-    return b(this, p, E).call(this, e, r);
+    const r = d(this, u)[e] = (d(this, u)[e] || 0) + 1;
+    return b(this, w, v).call(this, e, r);
   }
   /**
    * Wrap reading from object directly COUNT_MAP[msgType]
@@ -32,22 +32,22 @@ class I {
    * @returns How many times a request of type msgType has been called by any given promiser
    */
   messageId(e) {
-    return b(this, p, E).call(this, e, a(this, u)[e]);
+    return b(this, w, v).call(this, e, d(this, u)[e]);
   }
   get(e) {
-    return a(this, f).get(e);
+    return d(this, f).get(e);
   }
   set(e, r) {
-    return a(this, f).set(e, r);
+    return d(this, f).set(e, r);
   }
 }
-f = new WeakMap(), u = new WeakMap(), p = new WeakSet(), E = function(e, r) {
+f = new WeakMap(), u = new WeakMap(), w = new WeakSet(), v = function(e, r) {
   return `${e}#${r}`;
 };
-function L() {
+function R() {
   return typeof window < "u" && typeof Worker < "u";
 }
-function W([t, e]) {
+function U([t, e]) {
   if (!t)
     throw new Error("better-worker1.main.worker1: you passed 0 args lol");
   if (typeof t == "string")
@@ -67,34 +67,34 @@ function W([t, e]) {
     `better-worker1.main.checkArgs: invalid arguments [${t}, ${e}]`
   );
 }
-function $(t, e) {
-  return async function(o, l) {
-    const { messageType: n, params: s, transfers: i } = W([o, l]), c = e.increment(n);
-    return i && e.set(c, i), t.then((m) => m(...s));
+function W(t, e) {
+  return async function(s, l) {
+    const { messageType: n, params: o, transfers: i } = U([s, l]), c = e.increment(n);
+    return i && e.set(c, i), t.then((m) => m(...o));
   };
 }
-function j(t = !1) {
-  if (L()) {
+function $(t = !1) {
+  if (R()) {
     const e = new Worker(
       new URL(
         "worker1.mjs",
         import.meta.url
       ),
       { type: "module" }
-    ), r = new I(), o = e.postMessage.bind(e);
-    e.postMessage = (s, i) => {
+    ), r = new I(), s = e.postMessage.bind(e);
+    e.postMessage = (o, i) => {
       t && console.log(
         "better-worker1.main.w1thread: sending with ports:",
         i
       );
-      const c = r.get(r.messageId(s.type));
-      return c ? o(s, c) : o(s);
+      const c = r.get(r.messageId(o.type));
+      return c ? s(o, c) : s(o);
     };
     const l = globalThis.sqlite3Worker1Promiser.v2({
       worker: e
     });
-    let n = $(l, r);
-    return n.$worker = e, n.lazy = (...s) => d.promise(() => n(...s)), n;
+    let n = W(l, r);
+    return n.$worker = e, n.lazy = (...o) => a.promise(() => n(...o)), n;
   } else
     return t && console.warn(
       "better-worker1.main.worker1Thread: non-browser environment detected, returning stub function..."
@@ -103,33 +103,33 @@ function j(t = !1) {
 }
 let _ = null;
 function M(t = !1) {
-  return _ || (_ = j(t)), _;
+  return _ || (_ = $(t)), _;
 }
-function q(t) {
-  return new URL(
+function L(t) {
+  return window === void 0 ? new URL("file:///dev/null") : new URL(
     // namespace for extension in static folder
     "sqlite-ext/medfetch.vtab.mjs",
     // relative to source  : relative to static root
     self.location.origin
   );
 }
-class P extends S.TaggedError("medfetch.sqlite") {
+class j extends S.TaggedError("medfetch.sqlite") {
 }
 let y;
-function T() {
-  return d.promise(
+function P() {
+  return a.promise(
     () => new Promise((t, e) => {
-      const { port1: r, port2: o } = new MessageChannel(), l = new Worker(
+      const { port1: r, port2: s } = new MessageChannel(), l = new Worker(
         new URL(
           "../fetch-worker.mjs",
           import.meta.url
         ),
         { type: "module" }
-      ), n = (s) => {
+      ), n = (o) => {
         var i;
-        s.data === "fetch-ready" || ((i = s.data) == null ? void 0 : i.type) === "fetch-ready" ? (r.removeEventListener("message", n), t(r)) : (r.removeEventListener("message", n), e(
+        o.data === "fetch-ready" || ((i = o.data) == null ? void 0 : i.type) === "fetch-ready" ? (r.removeEventListener("message", n), t(r)) : (r.removeEventListener("message", n), e(
           new Error(
-            `Unexpected message: ${JSON.stringify(s.data)}`
+            `Unexpected message: ${JSON.stringify(o.data)}`
           )
         ));
       };
@@ -137,57 +137,57 @@ function T() {
         {
           type: "init"
         },
-        [o]
+        [s]
       );
     })
   );
 }
-function N(t, { trace: e = !1, filename: r, dbId: o } = {}) {
-  if (!L())
+function N(t, { trace: e = !1, filename: r, dbId: s } = {}) {
+  if (!R())
     return e && console.warn(
       "medfetch: non-browser environment detected, returning stub function..."
-    ), (n, ...s) => {
+    ), (n, ...o) => {
     };
-  const l = d.gen(function* () {
+  const l = a.gen(function* () {
     if (y)
       return y;
     {
       const n = M();
-      if (!o)
+      if (!s)
         if (r) {
           const { dbId: c } = yield* n.lazy("open", {
             vfs: "opfs",
             filename: r
           });
-          o = yield* d.fromNullable(c);
+          s = yield* a.fromNullable(c);
         } else {
           const { dbId: c } = yield* n.lazy("open");
-          o = yield* d.fromNullable(c);
+          s = yield* a.fromNullable(c);
         }
-      const s = yield* T(), { result: i } = yield* n.lazy(
+      const o = yield* P(), { result: i } = yield* n.lazy(
         {
-          dbId: o,
+          dbId: s,
           type: "load-module",
           args: {
-            moduleURL: q().toString(),
+            moduleURL: L().toString(),
             moduleName: "medfetch",
             aux: new TextEncoder().encode(t)
           }
         },
-        [s]
+        [o]
       );
-      return i.rc !== 0 ? yield* new P({
-        message: `medfetch.sqlite: couldn't load in the module at ${q().toString()}`,
+      return i.rc !== 0 ? yield* new j({
+        message: `medfetch.sqlite: couldn't load in the module at ${L().toString()}`,
         type: "load-module"
-      }) : (y = o, y);
+      }) : (y = s, y);
     }
   });
-  return function(s, ...i) {
-    const c = s.reduce(
-      (m, h, R) => m + h + (i[R] ?? ""),
+  return function(o, ...i) {
+    const c = o.reduce(
+      (m, h, q) => m + h + (i[q] ?? ""),
       ""
     );
-    return d.gen(function* () {
+    return a.gen(function* () {
       const m = yield* l, { result: h } = yield* M().lazy({
         type: "exec",
         dbId: m,
@@ -202,6 +202,6 @@ function N(t, { trace: e = !1, filename: r, dbId: o } = {}) {
 }
 export {
   N as medfetch,
-  j as w1thread,
+  $ as w1thread,
   M as worker1
 };
