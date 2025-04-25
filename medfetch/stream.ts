cd783af5803c638009d81789decd4f6a9b8a0027
nathan <side_nathan@hotmail.com>
2025-04-25 19:22:47 -0400

    Add quick clarinet script to test

---
+++ medfetch/stream.ts
@@ -0,0 +1,66 @@
+import clarinet from "clarinet";
+
+const startURL = "https://r4.smarthealthit.org/Patient";
+const response = await fetch(startURL);
+const reader = response.body!.getReader();
+const decoder = new TextDecoder();
+
+const parser = clarinet.parser();
+let insideLinkArray = false;
+let insideLinkObject = false;
+let currentKey = "";
+let currentLink: any = {};
+
+parser.onkey = (key) => {
+    currentKey = key;
+
+    if (key === "link") {
+        insideLinkArray = true;
+    }
+};
+
+parser.onopenarray = () => {
+    // do nothing special yet
+};
+
+parser.onopenobject = (o) => {
+    if (insideLinkArray) {
+        insideLinkObject = true;
+        currentLink = {};
+    }
+};
+
+parser.onvalue = (value) => {
+    if (insideLinkArray && insideLinkObject) {
+        if (currentKey === "relation") {
+            console.log("got relation value:", value);
+        }
+        currentLink[currentKey] = value;
+    }
+};
+
+parser.oncloseobject = () => {
+    if (insideLinkArray && insideLinkObject) {
+        insideLinkObject = false;
+        console.log("Completed link object:", currentLink);
+    }
+};
+
+parser.onclosearray = () => {
+    if (insideLinkArray) {
+        insideLinkArray = false;
+        console.log("Done parsing link array");
+    }
+};
+
+while (true) {
+    const { value, done } = await reader.read();
+    if (done || !value) {
+        break;
+    }
+
+    const chunk = decoder.decode(value, { stream: true });
+
+    parser.write(chunk);
+}
+
